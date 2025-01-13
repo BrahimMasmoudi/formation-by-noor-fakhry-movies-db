f87b4068a6dd74464ab46fcb2ca632d7c1ca5c8b
brahim masmoudi <masmoudi.brahim@gmail.com>
2025-01-13 13:57:28 +0100

    Fix size cards (#3)
    * wip
    
    * delete tailwind
    
    * delete loader

--- pages/trending.vue
+++ pages/trending.vue
@@ -1,14 +1,12 @@
 <template>
   <Tabs v-model="selectedTab">
     <template #movie>
-     <Loader v-if="!elements"/>
       <v-col sm="6" lg="2" md="3" v-for="movie in elements?.movies?.results">
         <MovieCard :movie="movie"/>
       </v-col>
     </template>
 
     <template #tv>
-      <Loader v-if="!elements"/>
       <v-col sm="6" lg="2" md="3" v-for="series in elements?.series?.results">
         <SeriesCard :series="series"/>
       </v-col>
