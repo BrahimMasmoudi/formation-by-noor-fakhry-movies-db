311f4e028257ea48c6d39ecf01cb1d704b9dcb2d
BrahimMasmoudi <masmoudi.brahim@gmail.com>
2025-01-13 10:51:48 +0100

    use tabs in header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
-    ssr : true,
+    ssr: true,
     devtools: {enabled: true},
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: ['@nuxtjs/tailwindcss',
+        'vuetify-nuxt-module',
+    ],
     app: {
         head: {
             link: [
@@ -19,7 +21,36 @@ export default defineNuxtConfig({
             ]
         }
     },
-    runtimeConfig : {
+    runtimeConfig: {
         accessToken: process.env.ACCESS_TOKEN
+    },
+    vuetify: {
+        vuetifyOptions: {
+            theme: {
+                themes: {
+                    light: {
+                        dark: false,
+                        colors: {
+                            primary: '#3643BA',
+                            "dark-primary": '#2E399E',
+                            gray: '#a0a0a0',
+                            "quiet-gray": '#616161',
+                            'dark-gray': '#4E5D6BCC',
+                            "light-gray": '#F5F5F5',
+                            "primary-light": '#D9ECF5',
+                            "light-blue": '#9ADDFF',
+                            'completed': '#149B65',
+                            'in_progress': '#0113B6FF',
+                            'to_be_processed': '#EC6607',
+                            archive: '#D70321',
+                            yes: '#149B65',
+                            no: '#ff0000',
+                            'not-concerned': '#3643BA'
+                        }
+                    },
+                }
+            },
+        }
     }
+
 })
